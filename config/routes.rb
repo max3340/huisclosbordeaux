318f368001da586077f184163bea99a2114d290c
maxime garcia <maxime-garcia@windowslive.com>
2020-05-04 13:45:15 +0200

    ok

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :projets
+  resources :souscategories
+  resources :categories
+
+  get '/admin', to: 'users#admin', as: :admin_root
+
 end
